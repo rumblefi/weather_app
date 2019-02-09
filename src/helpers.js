4c958ea3888d6e69db3ddc7ac0af06ea3fa5f8ff
Yuriy Bahur <yuriybahur1@gmail.com>
2019-02-09 18:06:26 +0200

    hfjds

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,32 @@
 export const getDayName = (date = new Date()) => {
-	const days = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
-	return days[date.getDay()]
+    const days = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+    ]
+    return days[date.getDay()]
 }
 
 export const getMonthName = (date = new Date()) => {
-	const months = ['Января','Февраля','Марта','Апреля','Мая','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря']
-	return months[date.getMonth()]
+    const months = [
+        'Января',
+        'Февраля',
+        'Марта',
+        'Апреля',
+        'Мая',
+        'Июня',
+        'Июля',
+        'Августа',
+        'Сентября',
+        'Октября',
+        'Ноября',
+        'Декабря'
+    ]
+    return months[date.getMonth()]
 }
 
 export const getDate = (date = new Date()) => date.getDate()
