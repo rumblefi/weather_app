9416e6ddf9cc6e0c5500749a240d5456d6804f39
Yuriy Bahur <yuriybahur1@gmail.com>
2019-02-09 17:46:34 +0200

    hfjds

---
+++ src/components/Day/Day.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {getDayName,getMonthName,getDate} from '../../helpers'
+
+const Day = ({ dayData : {day,date}, icons }) => {
+
+	return(
+
+		<div className="days__item">
+			
+			<i className={`wi ${icons[day.condition.code]} days__icon`}></i>
+			
+			<div className="days__week-day">{getDayName( new Date(date) )}</div>
+
+			<ul className="days-list">
+			
+				<li className="days-list__item">{day.maxtemp_c}°C</li>
+
+				<li className="days-list__item">{day.mintemp_c}°C</li>
+
+			</ul>
+
+		</div>
+
+	)
+
+}
+
+export default Day
--- src/components/Days/Days.js
+++ src/components/Days/Days.js
@@ -1,27 +1,15 @@
 import React from 'react'
 import './Days.css'
-import {getDayName,getMonthName,getDate} from '../../helpers'
+import Day from '../Day/Day'
 
 const Days = ({daysData,icons}) => {
 
-	const daysItems = daysData.map( ({ date_epoch, date, day:{maxtemp_c,mintemp_c,condition} }) => {
+	const daysItems = daysData.map( (dayData) => {
+
+		console.log(dayData)
 
 		return(
-			<div key = {date} className="days__item">
-			
-				<i className={`wi ${icons[condition.code]} days__icon`}></i>
-				
-				<div className="days__week-day">{getDayName( new Date(date) )}</div>
-
-				<ul className="days-list">
-				
-					<li className="days-list__item">{maxtemp_c}°C</li>
-
-					<li className="days-list__item">{mintemp_c}°C</li>
-
-				</ul>
-
-			</div>
+			<Day key = {dayData.date}  dayData = {dayData}  icons = {icons} />
 		)
 
 	}) 
